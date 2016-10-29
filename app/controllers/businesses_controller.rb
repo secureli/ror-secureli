b515c1fb83b9c3d9f3ff5e8362ebbae7af1b7260
salman aslam <salman@clustox.com>
2016-10-30 00:42:04 +0500

    added search address info

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -3,23 +3,35 @@ require 'net/https'
 
 class BusinessesController < ApplicationController
   def index
-  	if params[:googl_api_key].present? && params[:lat].present? && params[:lng].present?
-  		@output = search_business(params[:googl_api_key], params[:output_type], params[:lat], params[:lng] , params[:keyword])
-  	end
+  	@output = ''
+  	if params[:type] == 'Business'
+	  	if params[:googl_api_key].present? && params[:lat].present? && params[:lng].present?
+	  		#@output = search_business(params[:googl_api_key], params[:output_type], params[:lat], params[:lng] , params[:keyword])
+	  		url = "https://maps.googleapis.com/maps/api/place/radarsearch/" +  params[:output_type] + "?key=" + params[:googl_api_key] + "&location=" + params[:lat] + "," + params[:lng] + "&keyword=" + params[:keyword] + "&radius=50000"
+	  		@output = search_business(url)
+	  	end
+	  elsif params[:type] == 'Address'
+	  	url = "https://maps.googleapis.com/maps/api/geocode/" + params[:output_type] + "?key=" + params[:googl_api_key] + "&address=" + params[:address]
+	  	@output = search_business(url)
+	  end
+  end
+
+  def new
   end
 
   def show
   end
 
-  def search_business(google_key="", output="", lat="", lng="" , keyword="")
+  def search_business(url)
   	results = { }
-  	url = "https://maps.googleapis.com/maps/api/place/radarsearch/" + output + "?key=" + google_key + "&location=" + lat + "," + lng + "&keyword=" + keyword + "&radius=50000"
+  	#url = "https://maps.googleapis.com/maps/api/place/radarsearch/" + output + "?key=" + google_key + "&location=" + lat + "," + lng + "&keyword=" + keyword + "&radius=50000"
+  	puts url
   	uri = URI.parse(url)
   	http = Net::HTTP.new(uri.host, uri.port)
   	http.open_timeout = 30
-	http.read_timeout = 30
-	http.use_ssl = true
-	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		http.read_timeout = 30
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   	response = http.request_post("#{uri.path}?#{uri.query}", "")
 
 	case response
